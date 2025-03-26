daed1c8e6479f401215cd9802f56086247c11a23
Amirali Motahari <117802865+AmiraliMotahari@users.noreply.github.com>
2025-03-26 20:13:45 +0330

    v0.0.5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         >
           <Header />
           <main>{children}</main>
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { Github, Heart, Linkedin } from "lucide-react";
+import { Button } from "./ui/button";
+
+const currentYear = new Date().getFullYear();
+
+const socials = [
+  {
+    title: "LinkedIn",
+    icon: Linkedin,
+    href: "https://www.linkedin.com/in/amirali-motahari-252a651a7",
+  },
+  {
+    title: "GitHub",
+    icon: Github,
+    href: "https://github.com/AmiraliMotahari",
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer className="border-t py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          {/* Logo */}
+          <div className="mb-4 md:mb-0">
+            <Link
+              href="/"
+              className="text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-neon-green to-neon-red"
+            >
+              Portfolio
+            </Link>
+          </div>
+
+          {/* Copy Right */}
+          <div className="text-sm text-muted-foreground">
+            <p className="flex items-center justify-center md:justify-end">
+              © {currentYear} | Made with{" "}
+              <Heart className="h-4 w-4 mx-1 text-neon-red" /> by Me
+            </p>
+          </div>
+
+          {/* Socials */}
+          <div className="flex justify-center items-center gap-2">
+            {socials.map((social) => {
+              return (
+                <Button key={social.title} asChild size={"icon"} variant={"ghost"}>
+                  <Link href={social.href}>
+                    <social.icon />
+                  </Link>
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
